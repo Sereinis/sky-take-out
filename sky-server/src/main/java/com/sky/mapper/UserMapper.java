9acca386aabf1901d01f322a570c563c6bec8463
Sereinis <3528698343@qq.com>
2026-07-24 10:42:29 +0800

    微信登录及商品展示业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.entity.User;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    /*根据用户openid查询用户*/
+    @Select("select * from user where openid = #{openid}")
+    User getByOpenid(String openid);
+
+    void insert(User user);
+}
